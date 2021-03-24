0c64282e549746f2ded2e85f246a222abdc0b7db
wangdengzhi <1303465909@qq.com>
2021-03-24 10:18:41 +0800

    [feature][what]封装第二种插件模板

--- assets/git-hooks/templates/template1/readme.md
+++ assets/git-hooks/templates/template1/readme.md
@@ -1,4 +1,5 @@
 # 简介
 
 默认模板
-配置 eslint 检测、commit 信息检测和 commitTag 标志
+
+配置 eslint 检测、commit 规范检测、开源协议检测和 commitTag 标志
---
+++ assets/git-hooks/templates/template2/commit-msg.js
@@ -0,0 +1,36 @@
+const AbstractHook = require('../AbstractHook');
+const GhCore = require('../../git-hooks-new.js').default;
+const process = require('process');
+
+class CommitMsgHook extends AbstractHook {
+  constructor() {
+    super();
+    this.core = new GhCore({});
+  }
+}
+
+const hook = new CommitMsgHook();
+hook.use(async (core, next) => {
+  const commitMsgStoragePath = process.argv.slice(-1)[0];
+  core['commit-message-plugin'].setCommitMsgStoragePath(commitMsgStoragePath);
+
+  // 读取 commit
+  const commitMsg = core['commit-message-plugin'].readCommitMessage();
+
+  // 注入对应字段
+  core.commitMsg = commitMsg;
+  await next();
+});
+
+hook.use(async (core, next) => {
+  const commitMsgCheckResult = core['commit-message-check-plugin'].standardChecker(core.commitMsg);
+  if (!commitMsgCheckResult.isPass) {
+    core.logger.error(
+      `commitMessage-不符合规范-缺少：+ ${commitMsgCheckResult.missKeywords.join(',')}`
+    );
+    process.exit(1);
+  }
+  await next();
+});
+
+hook.run();
